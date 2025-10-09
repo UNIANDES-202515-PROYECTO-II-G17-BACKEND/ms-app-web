cbd385e18f28e05e3948d44927b4a0fef45b95ba
Angel Racini <a.racini@uniandes.edu.co>
2025-10-08 21:31:11 -0500

    Add ESLint configuration and Karma setup for Angular project

---
+++ karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: { clearContext: false },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reporters: [
+        { type: 'json-summary', subdir: '.', file: 'coverage-final.json' },
+        { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
+      ],
+      fixWebpackSourcePaths: true
+    },
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: false
+  });
+};
